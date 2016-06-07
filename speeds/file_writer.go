7851e5b8fd9e9b4fa1df1a245a397769249ff420
miraclew <miraclew@gmail.com>
2016-06-07 12:02:01 +0800

    add file writer

---
+++ speeds/file_writer.go
@@ -0,0 +1,25 @@
+package speeds
+
+import (
+    "os"
+    //"fmt"
+    "fmt"
+)
+
+func writeChunk(file *os.File, offset int64, b []byte) error {
+    ret, err := file.Seek(offset, 0)
+    if err != nil {
+        return err
+    }
+
+    fmt.Printf("offset: %d\n" ,ret)
+    fmt.Println("write ", b)
+    for i := 0; i < 10; i++ {
+        _,err := file.Write(b)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
---
+++ speeds/file_writer_test.go
@@ -0,0 +1,31 @@
+package speeds
+
+import (
+    "os"
+    "testing"
+)
+
+func TestWriteChunk(t *testing.T) {
+    file, err := os.OpenFile("aaa.txt", os.O_RDWR | os.O_CREATE, 0666)
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+
+    defer file.Close()
+
+    _,err = file.Write([]byte{'x'})
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+
+    err = writeChunk(file, 10, []byte{'a'})
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+
+    err = writeChunk(file, 30, []byte{'b'})
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+}
+
